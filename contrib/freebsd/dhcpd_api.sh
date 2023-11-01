1d5f4ff8b0af0c0bdeca609fd9d193acb2c5cbb4
Dylan Hardison <dylan@hardison.net>
2023-10-31 21:35:12 -0700

    make freebsd init more robust
    I'm not a fan of this init system.

--- contrib/freebsd/dhcpd_api.sh
+++ contrib/freebsd/dhcpd_api.sh
@@ -27,9 +27,25 @@ procname="daemon"
 # shellcheck disable=SC2034
 start_cmd="dhcpd_api_start"
 
+# shellcheck disable=SC2034
+start_precmd="dhcpd_api_pre_start"
+
+dhcpd_api_pre_start()
+{
+    if ! id -u dhcpd_api >/dev/null 2>&1; then
+        pw adduser dhcpd_api -g dhcpd -m -d /var/db/dhcpd-api -s /usr/sbin/nologin -c 'dhcpd api user'
+    fi
+}
+
 dhcpd_api_start()
 {
-    daemon -S -r -f -u dhcpd_api -P "$pidfile" -R 10 /usr/local/bin/dhcpd-api &
+    if ! id -u dhcpd_api >/dev/null 2>&1; then
+        err 1 "User dhcpd_api does not exist."
+    fi
+    if ! pw user show dhcpd_api 2>/dev/null |cut -d: -f9 |grep -q /var/db/dhcpd-api; then
+        err 2 "User dhcpd_api has no home directory set."
+    fi
+    daemon -S -r -u dhcpd_api -P "$pidfile" -R 10 /usr/local/bin/dhcpd-api &
 }
 
 run_rc_command "$1"
